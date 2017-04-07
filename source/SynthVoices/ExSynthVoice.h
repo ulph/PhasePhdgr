d9b9a39e490fa4af49e49201dc250356f779ae44
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-04-08 01:04:53 +0200

    also Y axis for the ex connectiongraph

--- source/SynthVoices/ExSynthVoice.h
+++ source/SynthVoices/ExSynthVoice.h
@@ -66,6 +66,15 @@ namespace PhasePhckr {
           connectionGraph(48000),
           t(0)
         {
+          // approximation of the patch in ExSynthVoice. no noise, less envelope tweaks etc.
+          //
+          // we lack a _lot_ of nuances as it's a royal pain to scale, add bias, invert etc
+          // need to figure out a good way to streamline that as number of modules and 
+          // connections grows painfully fast
+          //
+          // also, ports summation on input is really needed, 
+          // or number of modules needed explodes for anything but the most trivial
+
           inBus = connectionGraph.addModule(new InputBusModule());
           outBus = connectionGraph.addModule(new OutputBusModule());
 
@@ -75,19 +84,41 @@ namespace PhasePhckr {
 
           int atan = connectionGraph.addModule("ATAN");
           int atanMul = connectionGraph.addModule("MUL");
+          int osc1 = connectionGraph.addModule("SINE");
           int osc2 = connectionGraph.addModule("SINE");
+          int osc3mul = connectionGraph.addModule("MUL");
+          int osc3 = connectionGraph.addModule("SINE");
+          int osc23 = connectionGraph.addModule("XFADE");
 
           connectionGraph.connect(inBus, 0, mixEnv, 0);
+          connectionGraph.connect(inBus, 1, mixEnv, 1);
+          connectionGraph.connect(inBus, 2, mixEnv, 5);
           connectionGraph.connect(inBus, 6, mixEnv, 4);
           connectionGraph.connect(inBus, 3, phase, 0);
 
-          connectionGraph.connect(phase, 0, atan, 0);
-          connectionGraph.connect(atan, 0, osc2, 0);
-          connectionGraph.connect(mixEnv, 0, atanMul, 0);
-          connectionGraph.getModule(atanMul)->setInput(1, 10); // cheating ...
-          connectionGraph.connect(atanMul, 0, atan, 1);
+          // osc 1, f0 cohesion...
+          connectionGraph.connect(phase, 0, osc1, 0);
 
-          connectionGraph.connect(osc2, 0, mul, 0);
+          // osc 2 and osc 3, crossfade on Y and atan prescale on Z (... env)
+          connectionGraph.connect(phase, 0, atan, 0);
+          connectionGraph.connect(mixEnv, 0, atanMul, 0);
+          connectionGraph.getModule(atanMul)->setInput(1, 10); // cheating!
+          connectionGraph.connect(atanMul, 0, atan, 1);
+          connectionGraph.connect(atan, 0, osc2, 0);
+
+          connectionGraph.connect(atan, 0, osc3mul, 0);
+          connectionGraph.getModule(osc3mul)->setInput(1, 2); // cheating, again!
+          connectionGraph.connect(osc3mul, 0, osc3, 0);
+
+          connectionGraph.connect(osc2, 0, osc23, 0);
+          connectionGraph.connect(osc3, 0, osc23, 1);
+          connectionGraph.connect(inBus, 5, osc23, 2);
+
+          // mix amplitude on Z (... env)
+          int mix = connectionGraph.addModule("ADD");
+          connectionGraph.connect(osc1, 0, mix, 0);
+          connectionGraph.connect(osc23, 0, mix, 1);
+          connectionGraph.connect(mix, 0, mul, 0);
           connectionGraph.connect(mixEnv, 0, mul, 1);
 
           connectionGraph.connect(mul, 0, outBus, 0);

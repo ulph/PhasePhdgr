29ad2f0de584614084c00c6d22ab9f08c0293406
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2017-04-07 12:17:25 +0200

    Added a test for ConnectionGraph

---
+++ test/test_connectiongraph.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdint>
+#include "connectiongraph.hpp"
+
+int main()
+{
+    uint32_t fs = 48000;
+    ConnectionGraph s(fs);
+    int osc1 = s.addModule("PHASE");
+    s.setInput(osc1, 0, 440.0f);
+    
+    int osc2 = s.addModule("PHASE");
+    s.setInput(osc2, 0, 0.5f);
+    
+    int square = s.addModule("SQUARE");
+    s.connect(osc1, square, 0);
+    s.connect(osc2, square, 1);
+    
+    int gain = s.addModule("MUL");
+    s.setInput(gain, 0, 0.25f);
+    s.connect(square, gain, 1);
+    
+    int q8 = s.addModule("QUANT8");
+    s.connect(gain, q8);
+
+    int speaker = s.addModule("CLAMP");
+    s.connect(q8, speaker, 0);
+    
+    for(uint32_t t = 0; t < 5*fs; t++) {
+        float output = s.getOutput(speaker, t);
+        fwrite(&output, sizeof(output), 1, stdout); 
+    }
+}

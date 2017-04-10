da2cb9ce77b29731fff5bfa3d85af5795f45ff41
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2017-04-10 12:47:01 +0200

    Added test for performance improvements

---
+++ test/test_performance.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdint>
+#include "SynthVoice.hpp"
+
+using namespace PhasePhckr;
+
+int main()
+{
+    float buffer[48000];
+    
+    ExConnectionGraphVoice v = ExConnectionGraphVoice();
+    
+    v.mpe.on(48, 0.5f);
+    
+    for(int i = 0; i < 10; i++) {
+        v.update(buffer, 48000, 48000);
+    }
+}

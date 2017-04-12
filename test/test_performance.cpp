245c85545fe7b887ab9c8630547567bae86060c2
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-04-12 18:27:39 +0200

    fixup on test_performance to clear buffers before each iteration

--- test/test_performance.cpp
+++ test/test_performance.cpp
@@ -13,10 +13,13 @@ int main()
     GlobalData g;
     
     v.mpe.on(48, 0.5f);
+    v.mpe.press(0.5);
     
-    for(int i = 0; i < 10*48000; i += SYNTH_VOICE_BUFFER_LENGTH) {
+    for(int i = 0; i < 1*48000; i += SYNTH_VOICE_BUFFER_LENGTH) {
+        memset(bufferL, 0, sizeof(float)*SYNTH_VOICE_BUFFER_LENGTH);
+        memset(bufferR, 0, sizeof(float)*SYNTH_VOICE_BUFFER_LENGTH);
         v.processingStart(SYNTH_VOICE_BUFFER_LENGTH, 48000, g);
         v.processingFinish(bufferL, bufferR, SYNTH_VOICE_BUFFER_LENGTH);
-        //fwrite(bufferL, sizeof(float), 48000, stdout);
+        fwrite(bufferL, sizeof(float), 48000, stdout);
     }
 }

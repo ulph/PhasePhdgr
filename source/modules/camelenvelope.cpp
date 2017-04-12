73a73226696531549b22b39ddd366b7ac06916e1
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-04-12 12:19:22 +0200

    power law for camels

--- source/modules/camelenvelope.cpp
+++ source/modules/camelenvelope.cpp
@@ -16,6 +16,13 @@ CamelEnvelope::CamelEnvelope():
     inputs.push_back(Pad("offBumpHeight", 0.25f));
     inputs.push_back(Pad("offAttackSpeed", 0.05f));
     inputs.push_back(Pad("offDecaySpeed", 0.05f));
+
+    // power laws for snappyness
+    inputs.push_back(Pad("onAttackPow", 2.0f));
+    inputs.push_back(Pad("onDecayPow", 0.5f));
+    inputs.push_back(Pad("offAttackPow", 2.0f));
+    inputs.push_back(Pad("offDecayPow", 0.5f));
+
 }
 
 void CamelEnvelope::process(uint32_t fs) {
@@ -28,6 +35,11 @@ void CamelEnvelope::process(uint32_t fs) {
     float offAttackSpeed = inputs[6].value;
     float offDecaySpeed  = inputs[7].value;
 
+    float onAttackPow    = inputs[8].value;
+    float onDecayPow     = inputs[9].value;
+    float offAttackPow   = inputs[10].value;
+    float offDecayPow    = inputs[11].value;
+
     float targetValue = 0;
 
     // reset counter if falling or rising edge on gate
@@ -35,20 +47,24 @@ void CamelEnvelope::process(uint32_t fs) {
         samplesCtr = 0;
     }
     gate = newGate;
+    float pow = 1.0;
 
     float envTime = (float)samplesCtr / (float) fs;
     if(gate){
         if(envTime < onAttackSpeed){
             // attack region
             targetValue = (sustainHeight + onBumpHeight) * (envTime / onAttackSpeed);
+            pow = onAttackPow;
         }
         else if(envTime < (onAttackSpeed + onDecaySpeed)){
             // decay region
             targetValue = sustainHeight + onBumpHeight * (1 - ((envTime - onAttackSpeed) / onDecaySpeed));
+            pow = onDecayPow;
         }
         else{
             // sustain region
             targetValue = sustainHeight;
+            pow = 1.f;
         }
         gateOnTargetValue = targetValue; // so release has a reference
     }
@@ -56,15 +72,18 @@ void CamelEnvelope::process(uint32_t fs) {
         if(envTime < offAttackSpeed){
             // release attack region
             targetValue = gateOnTargetValue + offBumpHeight * (envTime / offAttackSpeed);
+            pow = offAttackPow;
         }
         else if(envTime < (offAttackSpeed + offDecaySpeed)){
             // release decay region
             targetValue = (gateOnTargetValue + offBumpHeight) * (1 - ((envTime - offAttackSpeed) / offDecaySpeed));
+            pow = offDecayPow;
         }
         else{
             // closed region
             targetValue = 0;
             gateOnTargetValue = 0;
+            pow = 1.f;
         }
     }
 
@@ -72,5 +91,5 @@ void CamelEnvelope::process(uint32_t fs) {
 
     // limit and slew
     targetValue = targetValue > 1.f ? 1.f : targetValue < 0.f ? 0.f : targetValue;
-    outputs[0].value = slew*outputs[0].value + (1-slew)*targetValue;
+    outputs[0].value = slew*outputs[0].value + (1-slew)*powf(targetValue, pow);
 }

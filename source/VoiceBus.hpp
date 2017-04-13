e54a4d22cf6f496dd790a2b9f86b478b2b44ff08
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-04-14 01:00:55 +0200

    ups, time is too much

--- source/VoiceBus.hpp
+++ source/VoiceBus.hpp
@@ -18,7 +18,6 @@ public:
     void handleNoteZ(int channel, int note, float position, std::vector<SynthVoice*> &voices);
     void update();
     float findScopeVoiceHz(std::vector<SynthVoice*> &voices);
-    void reset();
 private:
     std::vector<NoteData*> notes;
     int getNoteDataIndex(int channel, int note);

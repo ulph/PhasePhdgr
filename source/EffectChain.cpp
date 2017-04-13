4987e9d8ca9cc4233b9b6c84811d46ec20c7df11
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-04-13 11:37:13 +0200

    tweaks to exmample fx chain to cause some movement over time

--- source/EffectChain.cpp
+++ source/EffectChain.cpp
@@ -39,14 +39,13 @@ EffectChain::EffectChain(){
                 // a cross feedback stereo delay
                 ModulePortConnection{{"outPreGain", "left"}, {"leftDelayHP", "in"}},
                 ModulePortConnection{{"outPreGain", "right"}, {"rightDelayHP", "in"}},
-
                 ModulePortConnection{{"leftDelay", "out"}, {"leftDelayHP", "in"}},
                 ModulePortConnection{{"rightDelay", "out"}, {"rightDelayHP", "in"}},
                 ModulePortConnection{{"leftDelayHP", "out"}, {"leftDelayLP", "in"}},
                 ModulePortConnection{{"rightDelayHP", "out"}, {"rightDelayLP", "in"}},
-
                 ModulePortConnection{{"leftDelayLP", "out"}, {"rightDelay", "in"}},
                 ModulePortConnection{{"rightDelayLP", "out"}, {"leftDelay", "in"}},
+                // - output and "wet" factor
                 ModulePortConnection{{"leftDelayLP", "out"}, {"delayGain", "left"}},
                 ModulePortConnection{{"rightDelayLP", "out"}, {"delayGain", "right"}},
                 ModulePortConnection{{"delayGain", "left"}, {"outPreGain", "left"}},
@@ -77,14 +76,14 @@ EffectChain::EffectChain(){
                 ModulePortValue{"delayLeftTime", "scale", 0.001f},
                 ModulePortValue{"delayRightTime", "scale", 0.001f},
                 // - feedback amount
-                ModulePortValue{"leftDelay", "gain", 0.6f},
-                ModulePortValue{"rightDelay", "gain", 0.6f},
+                ModulePortValue{"leftDelay", "gain", 0.56f},
+                ModulePortValue{"rightDelay", "gain", 0.62f},
                 // - lowpass of feedback loop (sounds good, but also AA ...)
-                ModulePortValue{"leftDelayLP", "freq", 8000.0f},
-                ModulePortValue{"rightDelayLP", "freq", 8000.0f},
+                ModulePortValue{"leftDelayLP", "freq", 8500.0f},
+                ModulePortValue{"rightDelayLP", "freq", 7500.0f},
                 // - lowpass of feeback loop (sounds good, but also AA ...)
-                ModulePortValue{"leftDelayHP", "freq", 400.0f},
-                ModulePortValue{"rightDelayHP", "freq", 400.0f},
+                ModulePortValue{"leftDelayHP", "freq", 350.0f},
+                ModulePortValue{"rightDelayHP", "freq", 450.0f},
                 // - effect "wet amount"
                 ModulePortValue{"delayGain", "gain", 0.5f},
             }

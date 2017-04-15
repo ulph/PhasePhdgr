54efd6f1b6017ed9239cb743cae5a73772c5bca7
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-04-15 19:17:48 +0200

    quick test to fractionally delay in the delay module by using a windowed sinc, coefficients should be in lookup form

--- source/modules/delay.cpp
+++ source/modules/delay.cpp
@@ -13,20 +13,46 @@ Delay::Delay()
     memset(buffer, 0, sizeof(buffer));
 }
 
+float sincf(float x){
+    return x!=0 ? sinf(x)/x : 1;
+}
+
 void Delay::process(uint32_t fs) {
+    int bufferSize = sizeof(buffer) / sizeof(float);
     float t = inputs[1].value;
     float g = inputs[2].value;
 
     // limit time ranges
     t = t > 5.0f ? 5.0f : t < 0.0f ? 0.0f : t;
-    // buffersize is calculated for 5s @ 96kHz ... so 192kHz etc may not work as expected
 
-    int bufferSize = sizeof(buffer) / sizeof(float);
+    const int N = 5;
+    int tapeSamples = (int)(t*fs) - N;
+    tapeSamples = tapeSamples < 0 ? 0 : tapeSamples;
 
-    int writePosition = (readPosition + (int)(t*fs)) % bufferSize;
-    buffer[writePosition] = g*inputs[0].value;
+    int writePosition = (readPosition + tapeSamples);
+    writePosition %= bufferSize;
+
+    // fractionally resample (needed for modulating time)
+    float frac = t*fs-(int)(t*fs);
+
+    // design a FIR from windowed sinc with fractional delay as an approx of ideal allpass
+    float coeffs[N] = {0};
+
+    float M = 0;
+    for(int m=0; m<N; m++){
+        float arg = (float)m-frac-((float)N-1.f)/2.f;
+        float hamming = 0.54f+0.46f*cosf( (2.f*M_PI*arg) / (float)(N-1) );
+        coeffs[m] = hamming*sincf(arg);
+        M += coeffs[m];
+    }
+
+    // apply it
+    for(int m=0; m<N; m++){
+        buffer[(writePosition+m)%bufferSize] += g*inputs[0].value * coeffs[m] / M;
+    }
+
     outputs[0].value = buffer[readPosition];
+    buffer[readPosition] = 0;
     readPosition++;
     readPosition %= bufferSize;
-
 }

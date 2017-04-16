b89db9a916b8ada4f8af805f78e9f4023adef762
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-04-16 21:01:03 +0200

    lookup table for the fractional sincs

--- source/modules/delay.cpp
+++ source/modules/delay.cpp
@@ -1,7 +1,46 @@
 #include <string.h>
-
+#include <assert.h>
 #include "delay.hpp"
 
+const int c_sincWindowSize = 5;
+const int c_fracSincTableSize = 100;
+
+float sincf(float x){
+    return x!=0 ? sinf(x)/x : 1;
+}
+
+template<int N_, int numFractions_>
+struct FractionalSincTable
+{
+    static_assert( (N_ % 2) != 0); // N must be odd
+    FractionalSincTable()
+        : N(N_)
+        , numFractions(numFractions_)
+        , coeffs()
+    {
+        for (auto i = 0; i < numFractions; ++i)
+        {
+            float frac = (float)i*1.0f/(float(numFractions));
+            float M = 0;
+            for (auto n = 0; n < N; ++n)
+            {
+                float arg = (float)n-frac-((float)N-1.f)/2.f;
+                float hamming = 0.54f+0.46f*cosf( (2.f*M_PI*arg) / (float)(N-1) );
+                coeffs[i][n] = hamming*sincf(arg);
+                M += coeffs[i][n];
+            }
+            // silly normalization
+            for (auto n = 0; n < N; ++n)
+            {
+                coeffs[i][n] /= M;
+            }
+        }
+    }
+    const int N;
+    const int numFractions;
+    float coeffs[numFractions_][N_];
+};
+const auto g_fracSincTable = FractionalSincTable<c_sincWindowSize, c_fracSincTableSize>();
 
 Delay::Delay() 
     : readPosition(0)
@@ -13,42 +52,43 @@ Delay::Delay()
     memset(buffer, 0, sizeof(buffer));
 }
 
-float sincf(float x){
-    return x!=0 ? sinf(x)/x : 1;
-}
-
 void Delay::process(uint32_t fs) {
-    int bufferSize = sizeof(buffer) / sizeof(float);
     float t = inputs[1].value;
     float g = inputs[2].value;
 
     // limit time ranges
     t = t > 5.0f ? 5.0f : t < 0.0f ? 0.0f : t;
 
-    const int N = 5;
-    int tapeSamples = (int)(t*fs) - N;
+    int tapeSamples = (int)(t*fs) - c_sincWindowSize;
     tapeSamples = tapeSamples < 0 ? 0 : tapeSamples;
 
-    int writePosition = (readPosition + tapeSamples);
-    writePosition %= bufferSize;
-
-    // fractionally resample (needed for modulating time)
-    float frac = t*fs-(int)(t*fs);
+    const int writePosition = (readPosition + tapeSamples);
 
     // design a FIR from windowed sinc with fractional delay as an approx of ideal allpass
-    float coeffs[N] = {0};
+    const float frac = t*fs-(int)(t*fs);
+    assert(frac < 1.0f && frac >= 0.0f);
 
-    float M = 0;
-    for(int m=0; m<N; m++){
-        float arg = (float)m-frac-((float)N-1.f)/2.f;
-        float hamming = 0.54f+0.46f*cosf( (2.f*M_PI*arg) / (float)(N-1) );
-        coeffs[m] = hamming*sincf(arg);
-        M += coeffs[m];
+    // linearly interpolate between neighbouring sets of coefficients
+    const float softIdx = frac * (float)c_fracSincTableSize;
+    assert(softIdx < c_fracSincTableSize);
+    const int tableIdx1 = (int)softIdx;
+    const int tableIdx2 = ((tableIdx1+1) < c_fracSincTableSize) ? (tableIdx1+1) : 0;
+    assert(tableIdx1 < c_fracSincTableSize);
+    assert(tableIdx2 < c_fracSincTableSize);
+
+    const float ratio = softIdx - tableIdx1;
+    assert(ratio < 1.0f && ratio >= 0.0f);
+
+    float coeffs[c_sincWindowSize] = {0.0f};
+    for(int n=0; n<c_sincWindowSize; n++){
+        coeffs[n] = (1 - ratio) * g_fracSincTable.coeffs[tableIdx1][n];
+        coeffs[n] += ratio * g_fracSincTable.coeffs[tableIdx2][n];
     }
 
-    // apply it
-    for(int m=0; m<N; m++){
-        buffer[(writePosition+m)%bufferSize] += g*inputs[0].value * coeffs[m] / M;
+    // apply it on to write buffer (running convolution)
+    int bufferSize = sizeof(buffer) / sizeof(float);
+    for(int n=0; n<c_sincWindowSize; n++){
+        buffer[(writePosition+n)%bufferSize] += g*inputs[0].value * coeffs[n];
     }
 
     outputs[0].value = buffer[readPosition];

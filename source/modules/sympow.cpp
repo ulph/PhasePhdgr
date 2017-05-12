f6a85f3b0f5f4b26008489d54c8169e08ab2a3cb
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-05-12 17:11:30 +0200

    fabs

--- source/modules/sympow.cpp
+++ source/modules/sympow.cpp
@@ -7,7 +7,7 @@ SymPow::SymPow() {
 }
 
 void SymPow::process(uint32_t fs) {
-    float v = abs(inputs[0].value);
+    float v = fabs(inputs[0].value);
     float sign = inputs[0].value >= 0 ? 1.f : -1.f;
     outputs[0].value = sign*powf(v, inputs[1].value);
 }

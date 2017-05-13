0e4ea0c9e8dc52e0f3096ba38abee6939b52a2fd
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-05-13 22:03:26 +0200

    settable VST3 output dir...

--- cmake/vst_sdk.cmake
+++ cmake/vst_sdk.cmake
@@ -1,5 +1,7 @@
 # Copy paste (and modify) from VST SDK, as JUCE did not play nice ...
 
+set(VST3_OUTPUT_DIR ${CMAKE_BINARY_DIR}/VST3 CACHE FILEPATH "Where to put the VST3")
+
 function(mod_smtg_set_exported_symbols target exported_symbols_file)
     if(MSVC)
         set_target_properties(${target} PROPERTIES LINK_FLAGS "/DEF:\"${exported_symbols_file}\"")
@@ -13,6 +15,7 @@ endfunction()
 function(mod_smtg_add_vst3plugin target sdkroot)
     set(sources ${ARGN})
     add_library(${target} MODULE ${sources})
+    set_target_properties(${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${VST3_OUTPUT_DIR})
     if(APPLE)
         set_target_properties(${target} PROPERTIES BUNDLE TRUE)
         if(XCODE)
@@ -29,5 +32,23 @@ function(mod_smtg_add_vst3plugin target sdkroot)
         EXECUTE_PROCESS( COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE ARCHITECTURE )
         set(target_lib_dir ${ARCHITECTURE}-linux)
         set_target_properties(${target} PROPERTIES PREFIX "")
+        set_target_properties(${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${VST3_OUTPUT_DIR}/${target}.vst3/Contents/${target_lib_dir}")
+        add_custom_command(TARGET ${target} PRE_LINK
+            COMMAND ${CMAKE_COMMAND} -E make_directory
+            "${VST3_OUTPUT_DIR}/${target}.vst3/Contents/Resources"
+        )
     endif()
+endfunction()
+
+function(mod_smtg_add_vst3_resource target input_file)
+    if (LINUX)
+        add_custom_command(TARGET ${target} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy
+            "${CMAKE_CURRENT_LIST_DIR}/${input_file}"
+            "${VST3_OUTPUT_DIR}/${target}.vst3/Contents/Resources/"
+        )
+    elseif(APPLE)
+        target_sources(${target} PRIVATE ${input_file})
+        set_source_files_properties(${input_file} PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
+    endif(LINUX)
 endfunction()

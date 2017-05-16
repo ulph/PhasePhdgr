3cb221c7dc2fd6b0e5205e463711fe97a2ca4615
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-05-17 01:09:03 +0200

    safety and sensible tweaks to chamberlin filter

--- source/modules/chamberlin.cpp
+++ source/modules/chamberlin.cpp
@@ -1,5 +1,6 @@
 #include "chamberlin.hpp"
 #include <math.h>
+#include "inlines.hpp"
 
 ChamberlinFilter::ChamberlinFilter() 
     : band(0.f), low(0.f), high(0.f)
@@ -15,9 +16,9 @@ ChamberlinFilter::ChamberlinFilter()
 
 void ChamberlinFilter::process(uint32_t fs) {
     float x = inputs[0].value;
-    float wc = inputs[1].value;
-    float q = inputs[2].value;
-    float f = 2 * sinf((float)M_PI*wc / (float)fs);
+    float wc = limit(inputs[1].value, 0.f, fs * 0.125f);
+    float q = 1.f - limit(inputs[2].value, 0.f, 1.f);
+    float f = 2.f * sinf((float)M_PI*wc / (float)fs);
     low += f * band;
     high = x - low - q*band;
     band += f*high;

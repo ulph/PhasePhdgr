c473eafb9bc764323967fca6ef08c1f29dfaa52d
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-11-11 00:20:25 +0100

    cleanup

--- JUCE/FileBrowser/FileEditorBundle.cpp
+++ JUCE/FileBrowser/FileEditorBundle.cpp
@@ -53,6 +53,86 @@ FileEditorBundle::FileEditorBundle(const string& name, const File& directory, Ti
     resized();
 }
 
+void FileEditorBundle::buttonClicked(Button * btn)  {
+    if (btn == &saveButton) {
+        if (!isValidFilename()) return;
+
+        File targetFile = makeFullFileFromFilenameLabel();
+        if (targetFile.exists()) return;
+
+        auto j = fetchJsonCallback();
+        storeJson(targetFile, j);
+        watcher.refresh();
+    }
+    else if (btn == &goToRootButton) {
+        watcher.setDirectory(fileRoot, true, true);
+    }
+    else if (btn == &goOneUpButton) {
+        auto currDir = watcher.getDirectory();
+        if (currDir.getFullPathName() == fileRoot.getFullPathName()) return;
+        auto oneUp = currDir.getParentDirectory();
+        watcher.setDirectory(oneUp, true, true);
+    }
+}
+
+void FileEditorBundle::selectionChanged()  {}
+
+void FileEditorBundle::fileClicked(const File &file, const MouseEvent &e)  {
+    // switch directory
+    if (file.exists() && !file.existsAsFile()) {
+        watcher.setDirectory(file, true, true);
+    }
+}
+
+void FileEditorBundle::fileDoubleClicked(const File &file)  {
+    if (!file.existsAsFile()) return; // nonexistant or a directory
+    json j = loadJson(file);
+    fileLoadedCallback(j);
+    filenameLabel.setText(file.getFileNameWithoutExtension(), sendNotificationAsync);
+}
+
+void FileEditorBundle::browserRootChanged(const File &newRoot)  {}
+
+void FileEditorBundle::invalidateSelection() {
+    filenameLabel.setText("", sendNotificationAsync);
+}
+
+void FileEditorBundle::resized() {
+    const int rowHeightPx = 30;
+    const int titleWidthPx = 100;
+    const int saveButtonWidthPx = 100;
+    const int dirButtonWidthPx = 30;
+
+    auto bounds = getBounds();
+    auto boundsHeight = bounds.getHeight();
+    auto boundsWidth = bounds.getWidth();
+
+    float rowHeight = 0.01f;
+    float titleWidth = 0.01f;
+    float saveButtonWidth = 0.01f;
+    float dirButtonWidth = 0.01f;
+
+    if (boundsHeight > 0) {
+        rowHeight = (float)rowHeightPx / (float)boundsHeight;
+    }
+
+    if (boundsWidth > 0) {
+        titleWidth = (float)titleWidthPx / (float)boundsWidth;
+        saveButtonWidth = (float)saveButtonWidthPx / (float)boundsWidth;
+        dirButtonWidth = (float)dirButtonWidthPx / (float)boundsWidth;
+    }
+
+    float filenameLabelWidth = 1.f - titleWidth - 2 * dirButtonWidth - saveButtonWidth;
+
+    titleLabel.setBoundsRelative(0.f, 0.f, titleWidth, rowHeight);
+    goToRootButton.setBoundsRelative(titleWidth, 0.f, dirButtonWidth, rowHeight);
+    goOneUpButton.setBoundsRelative(titleWidth + dirButtonWidth, 0.f, dirButtonWidth, rowHeight);
+    filenameLabel.setBoundsRelative(titleWidth + 2*dirButtonWidth, 0.f, filenameLabelWidth, rowHeight);
+    saveButton.setBoundsRelative(1.f - saveButtonWidth, 0.f, saveButtonWidth, rowHeight);
+
+    list.setBoundsRelative(0.f, rowHeight, 1.f, 1.f - rowHeight);
+}
+
 
 FileBrowserPanel::FileBrowserPanel(PhasePhckrAudioProcessor& p)
     : fileWatchThread("editorFileWatchThread")
--- JUCE/FileBrowser/FileEditorBundle.hpp
+++ JUCE/FileBrowser/FileEditorBundle.hpp
@@ -8,6 +8,8 @@
 
 #include "PatchEditor.hpp" // TODO for _stylize
 
+class PhasePhckrAudioProcessorEditor;
+
 typedef std::function<void(const nlohmann::json&)> ProvideJsonCallBack;
 typedef std::function<nlohmann::json(void)> GetJsonCallBack;
 
@@ -40,87 +42,19 @@ private:
     bool isValidFilename();
 
 public:
-    virtual void buttonClicked(Button * btn) override {
-        if (btn == &saveButton) {
-            if (!isValidFilename()) return;
-
-            File targetFile = makeFullFileFromFilenameLabel();
-            if (targetFile.exists()) return;
-
-            auto j = fetchJsonCallback();
-            storeJson(targetFile, j);
-            watcher.refresh();
-        }
-        else if (btn == &goToRootButton) {
-            watcher.setDirectory(fileRoot, true, true);
-        }
-        else if (btn == &goOneUpButton) {
-            auto currDir = watcher.getDirectory();
-            if (currDir.getFullPathName() == fileRoot.getFullPathName()) return;
-            auto oneUp = currDir.getParentDirectory();
-            watcher.setDirectory(oneUp, true, true);
-        }
-    }
-    virtual void selectionChanged() override {}
-    virtual void fileClicked(const File &file, const MouseEvent &e) override {
-        // switch directory
-        if (file.exists() && !file.existsAsFile()) {
-            watcher.setDirectory(file, true, true);
-        }
-    }
-    virtual void fileDoubleClicked(const File &file) override {
-        if (!file.existsAsFile()) return; // nonexistant or a directory
-        json j = loadJson(file);
-        fileLoadedCallback(j);
-        filenameLabel.setText(file.getFileNameWithoutExtension(), sendNotificationAsync);
-    }
-    virtual void browserRootChanged(const File &newRoot) override {}
-    
-    void invalidateSelection() {
-        filenameLabel.setText("", sendNotificationAsync);
-    }
+    virtual void buttonClicked(Button * btn) override;
+    virtual void selectionChanged() override;
+    virtual void fileClicked(const File &file, const MouseEvent &e) override;
+    virtual void fileDoubleClicked(const File &file) override;
+    virtual void browserRootChanged(const File &newRoot) override;
+    void invalidateSelection();
 
     FileEditorBundle(const string& name, const File& directory, TimeSliceThread& watchThread, ProvideJsonCallBack fileLoadedCallback, GetJsonCallBack fetchJsonCallback);
 
-    virtual void resized() override {
-        const int rowHeightPx = 30;
-        const int titleWidthPx = 100;
-        const int saveButtonWidthPx = 100;
-        const int dirButtonWidthPx = 30;
-
-        auto bounds = getBounds();
-        auto boundsHeight = bounds.getHeight();
-        auto boundsWidth = bounds.getWidth();
-
-        float rowHeight = 0.01f;
-        float titleWidth = 0.01f;
-        float saveButtonWidth = 0.01f;
-        float dirButtonWidth = 0.01f;
-
-        if (boundsHeight > 0) {
-            rowHeight = (float)rowHeightPx / (float)boundsHeight;
-        }
-
-        if (boundsWidth > 0) {
-            titleWidth = (float)titleWidthPx / (float)boundsWidth;
-            saveButtonWidth = (float)saveButtonWidthPx / (float)boundsWidth;
-            dirButtonWidth = (float)dirButtonWidthPx / (float)boundsWidth;
-        }
-
-        float filenameLabelWidth = 1.f - titleWidth - 2 * dirButtonWidth - saveButtonWidth;
-
-        titleLabel.setBoundsRelative(0.f, 0.f, titleWidth, rowHeight);
-        goToRootButton.setBoundsRelative(titleWidth, 0.f, dirButtonWidth, rowHeight);
-        goOneUpButton.setBoundsRelative(titleWidth + dirButtonWidth, 0.f, dirButtonWidth, rowHeight);
-        filenameLabel.setBoundsRelative(titleWidth + 2*dirButtonWidth, 0.f, filenameLabelWidth, rowHeight);
-        saveButton.setBoundsRelative(1.f - saveButtonWidth, 0.f, saveButtonWidth, rowHeight);
-
-        list.setBoundsRelative(0.f, rowHeight, 1.f, 1.f - rowHeight);
-    }
+    virtual void resized() override;
 
 };
 
-class PhasePhckrAudioProcessorEditor;
 
 class FileBrowserPanel : public Component {
 private:

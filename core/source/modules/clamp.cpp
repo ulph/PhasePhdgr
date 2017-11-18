14575884bd531b9f2257a271b55e9c70f244d47d
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-11-19 00:49:35 +0100

    fixup on clamp module (ancient)

--- core/source/modules/clamp.cpp
+++ core/source/modules/clamp.cpp
@@ -3,12 +3,17 @@
 Clamp::Clamp()
 {
     inputs.push_back(Pad("in"));
+    inputs.push_back(Pad("low", 0.f));
+    inputs.push_back(Pad("high", 1.f));
     outputs.push_back(Pad("clamp"));
 }
 
 void Clamp::process(uint32_t fs)
 {
-    outputs[0] = inputs[0];
-    if(outputs[0].value < -1.0f) outputs[0].value = -1.0f;
-    else if(outputs[0].value > 1.0f) outputs[0].value = 1.0f;
+    auto v = inputs[0].value;
+    auto lo = inputs[1].value;
+    auto hi = inputs[2].value;
+    if(v < lo) v = lo;
+    else if(v > hi) v = hi;
+    outputs[0].value = v;
 }

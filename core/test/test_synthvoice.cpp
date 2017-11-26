38de61634be065a3eda06c65aa72e71928933264
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-11-26 20:49:36 +0100

    tweak

--- core/test/test_synthvoice.cpp
+++ core/test/test_synthvoice.cpp
@@ -10,6 +10,7 @@ using namespace PhasePhckr;
 
 int main()
 {
+    const int s = 120;
     const float fs = 48000;
     float bufferL[SYNTH_VOICE_BUFFER_LENGTH];
     float bufferR[SYNTH_VOICE_BUFFER_LENGTH];
@@ -20,7 +21,7 @@ int main()
     v.mpe.on(48, 0.5f);
     v.mpe.press(0.5);
     
-    for (int j = 0; j < 30; j++) {
+    for (int j = 0; j < s; j++) {
         for (int i = 0; i < 1 * (int)fs; i += SYNTH_VOICE_BUFFER_LENGTH) {
             memset(bufferL, 0, sizeof(float)*SYNTH_VOICE_BUFFER_LENGTH);
             memset(bufferR, 0, sizeof(float)*SYNTH_VOICE_BUFFER_LENGTH);

7afba3d78487ac0bf5257c58add49bce5375de1d
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-11-27 00:43:33 +0100

    long test, much sample

--- core/test/test_synthvoice.cpp
+++ core/test/test_synthvoice.cpp
@@ -10,19 +10,19 @@ using namespace PhasePhckr;
 
 int main()
 {
-    const int s = 120;
+    const int s = 60 * 30; // ~30 minutes
     const float fs = 48000;
     float bufferL[SYNTH_VOICE_BUFFER_LENGTH];
     float bufferR[SYNTH_VOICE_BUFFER_LENGTH];
 
     SynthVoice v(PhasePhckr::getExampleVoiceChain(), PhasePhckr::ComponentRegister());
     GlobalData g;
-    
+
     v.mpe.on(48, 0.5f);
     v.mpe.press(0.5);
-    
+
     for (int j = 0; j < s; j++) {
-        for (int i = 0; i < 1 * (int)fs; i += SYNTH_VOICE_BUFFER_LENGTH) {
+        for (int i = 0; i < (int)fs; i += SYNTH_VOICE_BUFFER_LENGTH) {
             memset(bufferL, 0, sizeof(float)*SYNTH_VOICE_BUFFER_LENGTH);
             memset(bufferR, 0, sizeof(float)*SYNTH_VOICE_BUFFER_LENGTH);
             v.processingStart(SYNTH_VOICE_BUFFER_LENGTH, fs, g);

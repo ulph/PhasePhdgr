9923f763f6d9865f940a6ecc9ec415497a35bd6d
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2017-12-04 22:01:44 +0100

    fixups for hte noise module

--- core/source/modules/noise.cpp
+++ core/source/modules/noise.cpp
@@ -3,7 +3,7 @@
 #include "noise.hpp"
 
 Noise::Noise() {
-    inputs.push_back(Pad("bias", 2.f));
+    inputs.push_back(Pad("bias", 0.f));
     inputs.push_back(Pad("scale", 1.f));
     outputs.push_back(Pad("out"));
     val = rand() % UINT32_MAX;
@@ -11,7 +11,7 @@ Noise::Noise() {
 
 void Noise::process() {
     float rand = ((float)((val & 0x7fffffff) - 0x40000000)) * (float)(1.0 / 0x40000000);
-    outputs[0].value = inputs[1].value*rand - 2.f + inputs[0].value;
+    outputs[0].value = inputs[1].value*rand*0.5f - 1.f + inputs[0].value;
     val = val * 435898247 + 382842987;
 }
 

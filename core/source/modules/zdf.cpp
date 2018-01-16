865b85c5955573ed093ee8d59435719ed7296010
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2018-01-16 11:43:46 +0100

    fixed frequency scaling, also I think v_ is the correct highpass

--- core/source/modules/zdf.cpp
+++ core/source/modules/zdf.cpp
@@ -5,14 +5,18 @@ ZdfLp::ZdfLp() {
     inputs.push_back(Pad("x"));
     inputs.push_back(Pad("wc", 16000.f, "hz"));
     outputs.push_back(Pad("y"));
+    outputs.push_back(Pad("v"));
 }
 
 void ZdfLp::process() {
     float x = inputs[0].value;
-    float wc = limitLow(inputs[1].value);
-    float g = prewarp(wc, fs, fsInv) * fsInv * 0.5f;
-    float v = (x - z1) * g / (1.0f + g);
+    float fc = limit(inputs[1].value, 1.0f, fs*0.5f);
+    float wc = fc*fsInv*2.0f*M_PI;
+    float g = prewarp(wc) * 0.5f;
+    float v_ = (x - z1) / (1.0f + g);
+    float v = v_*g;
     float y = v + z1;
     z1 = y + v;
     outputs[0].value = y;
+    outputs[1].value = v_;
 }
--- core/source/modules/zdf.hpp
+++ core/source/modules/zdf.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
 #include "module.hpp"
+
+#define _USE_MATH_DEFINES
 #include <math.h>
+#include <assert.h>
 
 // Bilinear transposed direct form II, aka TPT, transformed digital filters
 
-inline float prewarp(float wc, float fs, float fsInv) {
-    return 2.0f * fs * tanf(wc * fsInv * 0.5f);
+inline float prewarp(float wc) {
+    // nyquist is at M_PI;
+    assert(wc >= 0.0f);
+    assert(wc <= M_PI);
+    return 2.0f * tanf(wc * 0.5f);
 }
 
 class ZdfLp : public ModuleCRTP<ZdfLp> {

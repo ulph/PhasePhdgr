db5814c4ce7c86bc63fe2d8fa2d8a13dec569258
Ulf Hammarqvist <ulf.hammarqvist@pm.me>
2026-01-29 15:54:51 +0100

    refactor

--- core/test/blitosc_test.cpp
+++ core/test/blitosc_test.cpp
@@ -9,17 +9,7 @@
 #include <map>
 #include <set>
 
-auto generateDoubleSequence(auto randomGen, size_t period, size_t offset, size_t length) {
-    std::vector<double> seq(length);
-    auto val = randomGen();
-    for(auto n=0u; n<seq.size(); ++n) {
-        if(((n+offset)%period) == 0) {
-            val = randomGen(); 
-        }
-        seq[n] = val;
-    }
-    return seq;
-}
+#include "module_fuzzer.hpp"
 
 TEST_CASE( "blitosc", "[module]" ) {
     auto ma = ModuleAccessor();
@@ -41,34 +31,6 @@ TEST_CASE( "blitosc", "[module]" ) {
         REQUIRE(ma.getOutputs(module)[2].name == "integral");
     }
 
-    SECTION("process fuzzed") {
-        auto seed = GENERATE(2, 99);
-        auto period = GENERATE(1, 128);
+    moduleFuzzerTest(ma, module);
 
-        const auto length = 1024;
-
-        std::mt19937 gen(seed);
-        std::uniform_real_distribution<double> dist(-1.0, 1.0);
-        std::map<int, std::vector<double>> inputValues;
-        std::map<int, std::vector<double>> outputValues;
-
-        for(auto i=0u; i<ma.getInputs(module).size(); ++i){
-            inputValues[i] = generateDoubleSequence([&gen, &dist](){ return dist(gen); }, period, i, length);
-        }
-
-        for(auto o=0u; o<ma.getOutputs(module).size(); ++o){
-            outputValues[o] = std::vector<double>(length);
-        }
-
-        for (auto n=0u; n<length; ++n) {
-            for(auto i=0u; i<ma.getInputs(module).size(); ++i){
-                ma.setInput(module, i, inputValues[i][n]);
-            }
-            module.process();
-            for(auto o=0u; o<ma.getOutputs(module).size(); ++o){
-                outputValues[o][n] = ma.getOutput(module, o);
-            }
-        }
-
-    }
 }

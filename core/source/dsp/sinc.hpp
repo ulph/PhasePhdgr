da95dbccd8e7ee3e04e39fbd9d86d156e6a76bd0
Ulf Hammarqvist <ulf.hammarqvist@pm.me>
2026-01-30 10:28:33 +0100

    make sinc interpolation double precision and fixup some potential errors in tabel calculation. ensure, we also have _odd_ number of taps or stuff gets wonky

--- core/source/dsp/sinc.hpp
+++ core/source/dsp/sinc.hpp
@@ -2,14 +2,19 @@
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <cmath>
 
 #include <assert.h>
 
-static inline float sincf(float x){
-    return x!=0 ? sinf(x)/x : 1;
+static inline double sinc(double x) {
+    return x!=0 ? sin(x)/x : 1;
 }
 
-const int c_numFractions = 1000;
+static inline double nsinc(double x) {
+    return x!=0 ? sin(M_PI*x)/(M_PI*x) : 1;
+}
+
+const int c_numFractions = 1000; // TODO make templated arg
 
 static inline int makeIndex(int N, int n, int i) {
     return i*N + n;
@@ -27,35 +32,23 @@ template <int N>
 struct FractionalSincTable
 {
 protected:
-    float *coeffs;
-    const float normFreq;
-    const bool normAmp;
+    double *coeffs;
 
 public:
-
-    FractionalSincTable(float normFreq = (float)M_PI, bool normAmp = true)
-        : coeffs(new float[c_numFractions*N])
-        , normFreq(normFreq)
-        , normAmp(normAmp)
+    explicit FractionalSincTable()
+        : coeffs(new double[c_numFractions*N])
     {
         for (auto i = 0; i < c_numFractions; ++i)
         {
-            auto frac = (float)i*1.0f / (float(c_numFractions));
-            auto M = 0.f;
+            auto frac = (double)i*1.0 / (double)(c_numFractions);
             for (auto n = 0; n < N; ++n)
             {
                 auto ni = makeIndex(N, n, i);
-                auto arg = ((float)n - frac - ((float)N - 1.f) / 2.f);
+                auto arg = ((double)n - frac - ((double)N - 1.0) / 2.0);
                 auto w = blackman(arg, (double)N - 1.0);
-                coeffs[ni] = (float)w*sincf(arg * normFreq);
-                M += coeffs[ni];
-            }
-            if (normAmp) {
-                for (auto n = 0; n < N; ++n)
-                {
-                    int ni = makeIndex(N, n, i);
-                    coeffs[ni] /= M;
-                }
+                assert(std::isfinite(w));
+                coeffs[ni] = (double)w*sinc(arg * M_PI);
+                assert(std::isfinite(coeffs[ni]));
             }
         }
     };
@@ -64,18 +57,18 @@ public:
         delete[] coeffs;
     };
 
-    int getCoefficientTablePointer(const float fraction, float** destinationBuffer, const int destinationBufferSize) const {
+    int getCoefficientTablePointer(const double fraction, double** destinationBuffer, const int destinationBufferSize) const {
         if (destinationBufferSize < N)
         {
             assert(0);
             return 0;
         }
-        if (fraction > 1.0f || fraction < 0.f) {
+        if (fraction > 1.0 || fraction < 0.0) {
             assert(0);
             return 0;
         }
 
-        const float softIdx = fraction * (float)c_numFractions;
+        const double softIdx = fraction * (double)c_numFractions;
         assert(softIdx < c_numFractions);
         const int i = (int)softIdx;
         assert(i < c_numFractions);
--- core/source/modules/blitosc.hpp
+++ core/source/modules/blitosc.hpp
@@ -7,26 +7,26 @@
 class BlitOsc : public ModuleCRTP<BlitOsc>
 {
 public:
-    static const int c_blitN = 16; // TODO, find a sweet spot or at least use min-phase sinc
+    static const int c_blitN = 15; // TODO, find a sweet spot or at least use min-phase sinc
 private:
-    float buf[c_blitN] = { 0.f };
+    double buf[c_blitN] = { 0.f };
     int bufPos = 0;
-    float cumSum = 0.0f;
-    float cumCumSum = 0.0f;
+    double cumSum = 0.0f;
+    double cumCumSum = 0.0f;
     int stage = 0;
-    float internalSyncPhase = 0.0f;
-    float internalPhase = 0.0f;
-    float last_cumSum = 0.0f;
-    float last_cumCumSum = 0.0f;
-    float last_resetSignal = 0.0f;
-    float last_softResetSignal = 0.0f;
-    inline void hardResetOnSignal(float resetSignal);
-    inline void softResetOnSignal(float resetSignal, float syncAmount, float nFreq, float shape);
-    inline void incrementClocks(float nFreq, float syncNFreq);
-    inline void blitOnePulse(float fraction, float multiplier);
-    inline void blitForward(float& phase, float nFreq, float shape, float pwm);
-    inline void integrateAndStore(float nFreq, float shape, float freq, float dcRemoval);
-    inline void syncPhase(float& phase, float& syncPhase, float syncAmount, float syncNFreq, float nFreq, float shape);
+    double internalSyncPhase = 0.0f;
+    double internalPhase = 0.0f;
+    double last_cumSum = 0.0f;
+    double last_cumCumSum = 0.0f;
+    double last_resetSignal = 0.0f;
+    double last_softResetSignal = 0.0f;
+    inline void hardResetOnSignal(double resetSignal);
+    inline void softResetOnSignal(double resetSignal, double syncAmount, double nFreq, double shape);
+    inline void incrementClocks(double nFreq, double syncNFreq);
+    inline void blitOnePulse(double fraction, double multiplier);
+    inline void blitForward(double& phase, double nFreq, double shape, double pwm);
+    inline void integrateAndStore(double nFreq, double shape, double freq, double dcRemoval);
+    inline void syncPhase(double& phase, double& syncPhase, double syncAmount, double syncNFreq, double nFreq, double shape);
 public:
     BlitOsc();
     void process();
